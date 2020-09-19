751dabd40aa38530fb34b5d2de54a1dfadbb195e
Knightmare-sama <51862748+Knightmare-sama@users.noreply.github.com>
2020-09-19 16:16:35 +0530

    Player collisions added

---
+++ Lucid Shock/Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionHandler : MonoBehaviour
+{
+    [SerializeField] float levelLoadDelay = 1f;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print("Collision Occured Boiiii");
+
+        StartDeath();
+    }
+
+    private void StartDeath()
+    {
+        SendMessage("OnPlayerDeath");
+    }
+}
