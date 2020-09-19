0f0b2a8da1ada9d973591eb8f0974a441b392472
Knightmare-sama <51862748+Knightmare-sama@users.noreply.github.com>
2020-09-19 17:06:23 +0530

    llvel reload works

--- Lucid Shock/Assets/Scripts/CollisionHandler.cs
+++ Lucid Shock/Assets/Scripts/CollisionHandler.cs
@@ -7,17 +7,26 @@ using UnityEngine.SceneManagement;
 public class CollisionHandler : MonoBehaviour
 {
     [SerializeField] float levelLoadDelay = 1f;
+    [SerializeField] GameObject deathVFX;
 
 
     private void OnTriggerEnter(Collider other)
     {
         print("Collision Occured Boiiii");
-
         StartDeath();
     }
 
     private void StartDeath()
     {
         SendMessage("OnPlayerDeath");
+        deathVFX.SetActive(true);
+        Invoke("RestartLevel", levelLoadDelay);
+        
     }
+
+    void RestartLevel() //String Referenced!!
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
 }
