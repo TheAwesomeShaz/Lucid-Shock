f1e98de708c0cd4d441a7b3088eff4f5551e3b7f
Knightmare-sama <51862748+Knightmare-sama@users.noreply.github.com>
2020-09-24 11:56:53 +0530

    add scoreboardd

---
+++ Lucid Shock/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    private void Awake()
+    {
+        if(FindObjectsOfType<MusicPlayer>().Length > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
